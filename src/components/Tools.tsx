dc45a9da5b658841c18d37c978abc64f8694385d
Fazilfazi991 <zorxdxb@gmail.com>
2026-02-09 09:15:12 +0400

    Update tool logos and fill blank area in Why Choose Us section

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './Tools.css';
 
 const tools = [
-    { name: "Google Analytics", logo: "https://upload.wikimedia.org/wikipedia/commons/w/w/Google_Analytics_Logo.svg" },
+    { name: "Google Analytics", logo: "/brands/google-analytics.svg" },
     { name: "Google Ads", logo: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Google_Ads_logo.svg" },
     { name: "Meta Ads", logo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Meta_Platforms_Inc._logo.svg" },
-    { name: "Semrush", logo: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Semrush_logo.svg" },
-    { name: "Ahrefs", logo: "https://cdn.worldvectorlogo.com/logos/ahrefs.svg" },
-    { name: "HubSpot", logo: "https://upload.wikimedia.org/wikipedia/commons/d/d4/HubSpot_Logo.svg" },
-    { name: "Mailchimp", logo: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Mailchimp_freddie_icon.svg" },
+    { name: "Semrush", logo: "https://static.semrush.com/logo/semrush_logo_800.png" },
+    { name: "Ahrefs", logo: "https://cdn.ahrefs.com/images/logo/logo_180x40.png" },
+    { name: "HubSpot", logo: "/brands/hubspot.svg" },
+    { name: "Mailchimp", logo: "/brands/mailchimp.svg" },
     { name: "Shopify", logo: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Shopify_logo_2018.svg" },
     { name: "WordPress", logo: "https://upload.wikimedia.org/wikipedia/commons/9/98/WordPress_blue_logo.svg" },
-    { name: "Canva", logo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg" },
+    { name: "Canva", logo: "/brands/canva.svg" },
     { name: "Figma", logo: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg" },
     { name: "Adobe CC", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Creative_Cloud.svg/1200px-Creative_Cloud.svg.png" }
 ];
